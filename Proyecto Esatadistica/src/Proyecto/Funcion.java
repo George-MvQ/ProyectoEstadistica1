16ff078460762cdd81806c975af981df8e4e4fc1
George-MvQ <gvasquezq2@miumg.edu.gt>
2021-10-08 21:48:05 -0600

    Primer Commit

---
+++ Proyecto Esatadistica/src/Proyecto/Funcion.java
@@ -0,0 +1,38 @@
+
+package Proyecto;
+import java.math.BigInteger;
+
+
+
+/**
+ *
+ * @author George Vasquez
+ */
+public class Funcion {
+    
+    long factorial(long n){
+    
+        
+        
+        if (n==1||n==0){
+        return 1;
+        }
+        else{
+          return n*factorial(n-1); 
+        }  
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
+    
+
+
+
+
+
